f55cb895fdcfc3bafdb7b611ea9c06c97ff7b5c2
Rogerio Saraceni <rsaraceni@gmail.com>
2023-12-08 16:59:14 -0300

    fisrt commit

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# onpaste-js
+# Onpaste JS
+
+<h6>Onpaste JS</h6>
+<ul>
+    <li>Onpaste JS: v1.0.0</li>
+    <li>Version Date: 08/12/2023</li>
+</ul>
+
+<h6>Vendors</h6>
+<ul>
+    <li>
+        <a href="https://getbootstrap.com/" target="_blank">Bootstrap v5.3.2</a>
+    </li>
+    <li>
+        <a href="https://icons.getbootstrap.com/" target="_blank">Bootstrap Icons v1.11.2</a>
+    </li>
+    <li>
+        <a href="https://icons.getbootstrap.com/" target="_blank">fancybox v5.0</a>
+    </li>
+</ul>
+
+<p>
+    To copy and paste your image, use control+c and control+v or copy and paste with the mouse.
+</p>
+
+
+
+
+
+<p>
+    Developed by Rogério Saraceni.
+</p>
---
+++ assets/onpaste/onpaste.js
@@ -0,0 +1,56 @@
+/*!
+ * Onpaste JS:   v1.0.0
+ * Version Date: 08/12/2023
+ * Developed by: Rogério Saraceni
+ * Licensed MIT
+ */
+
+window.addEventListener("load", () => { 
+    function onpaste() {
+        const reader     = new FileReader();
+        const contentImg = document.querySelector('[data-js="img"]');
+        const inputImg   = document.querySelector('[data-js="input"]');
+        const btnZoom    = document.querySelector('[data-js="btn-zoom"]');
+        const btnDel     = document.querySelector('[data-js="btn-del"]');
+
+        reader.onload = function (result) {
+            const img = document.createElement("img");
+            img.src = result.target.result;
+
+            contentImg.appendChild(img);
+            contentImg.classList.add("z-2");
+            contentImg.setAttribute("data-src", img.src);
+            btnDel.classList.add("visible");
+            btnZoom.classList.add("d-flex");
+
+            btnDel.addEventListener("click", (event) => {
+                event.preventDefault();
+                img.remove();
+                contentImg.classList.remove("z-2");
+                contentImg.setAttribute("data-src", "");
+                btnDel.classList.remove("visible");
+                btnZoom.classList.remove("d-flex");
+            });
+        };
+
+        if (inputImg) {
+            inputImg.onpaste = function (event) {
+                if (contentImg.querySelector("img")) {
+                    return;
+                }
+
+                let items = event.clipboardData.items;
+
+                for (index in items) {
+                    let item = items[index];
+
+                    if (item.kind == "file") {
+                        reader.readAsDataURL(item.getAsFile());
+                    }
+                }
+            };
+        }
+    }
+    
+    onpaste()
+})
